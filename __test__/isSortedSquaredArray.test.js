cda3ffbabde3538e90a74c5e3b446b0f2541b1a5
Matthew Alvarez <mattalvarez354@yahoo.com>
2023-09-23 15:11:26 -0400

    refactored test suite

--- __test__/isSortedSquaredArray.test.js
+++ __test__/isSortedSquaredArray.test.js
@@ -1,33 +1,29 @@
 const sortedSquaredArray = require("../sortedSquaredArray");
 
-describe("isSortedSquaredArray", () => {
-  it("Should return a new Array with the sqrt values of the original Array in ascending order", () => {
-    const array = [1, 2, 3, 5, 6, 8, 9];
-
-    expect(sortedSquaredArray(array)).toBe([1, 4, 9, 25, 36, 64, 81]);
+describe("sortedSquaredArray", () => {
+  it("should return an empty array when given an empty input array", () => {
+    const result = sortedSquaredArray([]);
+    expect(result).toEqual([]);
   });
 
-  it("Should return a new Array with the sqrt values of the original Array in ascending order", () => {
-    const array = [1];
-
-    expect(sortedSquaredArray(array)).toBe([1]);
+  it("should return a sorted array of squared values", () => {
+    const inputArray = [-4, -2, 0, 2, 4];
+    const expectedOutput = [0, 4, 4, 16, 16];
+    const result = sortedSquaredArray(inputArray);
+    expect(result).toEqual(expectedOutput);
   });
 
-  it("Should return a new Array with the sqrt values of the original Array in ascending order", () => {
-    const array = [1, 2];
-
-    expect(sortedSquaredArray(array)).toBe([1, 4]);
+  it("should handle an input array with negative values", () => {
+    const inputArray = [-3, -2, -1, 0, 1, 2, 3];
+    const expectedOutput = [0, 1, 1, 4, 4, 9, 9];
+    const result = sortedSquaredArray(inputArray);
+    expect(result).toEqual(expectedOutput);
   });
 
-  it("Should return a new Array with the sqrt values of the original Array in ascending order", () => {
-    const array = [1, 2, 3, 4, 5];
-
-    expect(sortedSquaredArray(array)).toBe([1, 4, 9, 16, 25]);
-  });
-
-  it("Should return a new Array with the sqrt values of the original Array in ascending order", () => {
-    const array = [0];
-
-    expect(sortedSquaredArray(array)).toBe([0]);
+  it("should maintain the original order for duplicate squared values", () => {
+    const inputArray = [-3, -2, -2, -1, 0, 0, 1, 2, 3];
+    const expectedOutput = [0, 0, 1, 1, 4, 4, 4, 9, 9];
+    const result = sortedSquaredArray(inputArray);
+    expect(result).toEqual(expectedOutput);
   });
 });
